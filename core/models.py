fa526355876c147da6fc5f08256d880a54a7dff1
Jung <jungeun.lak@gmail.com>
2020-07-29 17:31:16 +0900

    user(Profile), Farm모델 작성

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=20, default='비공개')
+    photo = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user
+
+
+class Farm(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    # Basic Animals
+    donkey = models.BooleanField(default=False)
+    dog = models.BooleanField(default=False)
+    cat = models.BooleanField(default=False)
+    rooster = models.BooleanField(default=False)
+
+class CustomAnimal(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
