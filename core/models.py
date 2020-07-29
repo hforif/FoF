40b594c3a1dfcadbd4fad1814b230905aa039005
yongda1028 <yundayoung1028@gmail.com>
2020-07-29 18:09:03 +0900

    Made Model

--- core/models.py
+++ core/models.py
@@ -14,12 +14,26 @@ class Profile(models.Model):
 
 class Farm(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, default='Fororo')
 
     # Basic Animals
     donkey = models.BooleanField(default=False)
     dog = models.BooleanField(default=False)
     cat = models.BooleanField(default=False)
     rooster = models.BooleanField(default=False)
+    chick = models.BooleanField(default=False)
+
 
 class CustomAnimal(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    photo = models.ImageField(blank=True, null=True)
+
+
+class Farmer(models.Model):
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
+    custom_animal = models.ForeignKey(CustomAnimal, on_delete=models.CASCADE)
+    is_placed = models.BooleanField(default=False)
+
+
+
