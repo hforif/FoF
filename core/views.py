9aa3b877adc239b402c35e4241183176b8fd6d54
Jung <jungeun.lak@gmail.com>
2020-07-30 16:42:20 +0900

    feat(animal): show user's all animal_list

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from core.models import Profile, CustomAnimal
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+def show_animal_list(request):
+    user = User.objects.get(username='asus')
+    profile = Profile.objects.get(user=user)
+    animals = CustomAnimal.objects.filter(owner=profile)
+    return render(request, 'core/animal_list.html', {'animals': animals})
+
+
+def add_animal(request):
+    return render(request, 'core/animal_edit.html')
